25c17c975f5dea133f31174cdbf830c9a9fe6b3e
wrightrebecca <r3beccawright05@gmail.com>
2025-01-10 11:14:22 +0000

    Add search engine with event listener and access to inputted data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello </header>
+      <div className="container">
+        <header className="App-header">Hello </header>
+        <main>
+          <Dictionary />
+        </main>
+      </div>
     </div>
   );
 }
