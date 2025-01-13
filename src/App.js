434da8e72f23fe7b175d3d705be5835ff8f10336
wrightrebecca <r3beccawright05@gmail.com>
2025-01-13 11:48:28 +0000

    Embedded word meanings and word classes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">Hello </header>
+        <header className="App-header">React Dictionary</header> <br />
         <main>
           <Dictionary />
         </main>
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import Results from "./Results";
 import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
+
   function handleResponse(response) {
     console.log(response.data[0]);
+    console.log(response.data[0].meanings[0].definitions[0]);
+    setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for definition of "${keyword}"`);
 
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
@@ -25,6 +29,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" autoFocus={true} onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
