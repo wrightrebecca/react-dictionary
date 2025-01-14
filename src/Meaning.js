a5f0b2f895d947b936edeff5524ab060bf8fd5c3
wrightrebecca <r3beccawright05@gmail.com>
2025-01-14 11:44:30 +0000

    Incorporate phonetics

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,7 +5,7 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3> {props.meaning.partOfSpeech}</h3>
-      {props.meaning.definitions.map(function (definition, index) {
+      {props.meaning.definitions.slice(0, 2).map(function (definition, index) {
         return (
           <div key={index}>
             <p>
